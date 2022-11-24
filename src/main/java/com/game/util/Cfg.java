2acac854bda1b51ef9d0ff6109a1883c3b816053
Danila Ryabov <danila02478@gmail.com>
2022-11-24 20:11:08 +0300

    added p6spy

--- src/main/java/com/game/util/Cfg.java
+++ src/main/java/com/game/util/Cfg.java
@@ -4,8 +4,8 @@ public class Cfg {
     private static final String USER = "root";
     private static final String PASSWORD = "root";
     private static final String DIALECT = "org.hibernate.dialect.MySQL8Dialect";
-    private static final String URL = "jdbc:mysql://localhost:3306/rpg";
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:p6spy:mysql://localhost:3306/rpg";
+    private static final String DRIVER = "com.p6spy.engine.spy.P6SpyDriver";
 
     public static String getDIALECT() {
         return DIALECT;
